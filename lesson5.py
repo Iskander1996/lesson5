d1c5e975e690f15b03648921dbd58f7b8ef2eb29
iskander1996 <mukataev_1996@mail.ru>
2021-03-19 15:10:33 +0600

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,  начинающиеся с «#» будут проигнорированы, а пустое сообщение  отменяет процесс коммита.
     На ветке main
     Ваша ветка обновлена в соответствии с «origin/main».
    
     Изменения, которые будут включены в коммит:
    	изменено:      lesson5.py
    
     Неотслеживаемые файлы:
    	les_5_draw.py
    	lesson1_2.py
    	xdfd

--- lesson5.py
+++ lesson5.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 x = 1
+y = 10
+z = 100
+def f(x):
+    return np.log(np.exp(1/(np.sin(x)+1))/(5/4 + 1/(x**15)))/np.log(1+x**2)
 
-y = np.log(np.exp(1/(np.sin(x)+1))/(5/4 + 1/(x**15)))/np.log(1+x**2)
-
-print(y)
+print("f(1) = ",f(x))
+print("f(10) = ", f(y))
+print("f(100) =", f(z))
